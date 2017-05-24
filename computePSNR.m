3fd4f37995a4fa7868db52374fea149e28a3b457
siavashBigdeli <bigdeli@iam.unibe.ch>
2017-05-24 18:27:23 +0200

    Update computePSNR.m

--- computePSNR.m
+++ computePSNR.m
@@ -1,7 +1,18 @@
-function psnr = computePSNR(gt, map, pad)
+function psnr = computePSNR(im1, im2, pad)
+% Implements stochastic gradient descent (SGD) for maximum-a-posteriori for image super resolution described in:
+% In the paper "Image Restoration using Autoencoding Priors".
+%
+%
+% Input:
+% im1: First image in range of [0, 255].
+% im1: Second image in range of [0, 255].
+% pad: Scalar radious to exclude boundries from contributing to PSNR computation.
+%
+%
+% Output: PSNR
 
-gt_u = uint8(gt(pad+1:end-pad, pad+1:end-pad,:));
-map_u = uint8(map(pad+1:end-pad, pad+1:end-pad,:));
-imdff = double(map_u) - double(gt_u);
+im1_u = uint8(im1(pad+1:end-pad, pad+1:end-pad,:));
+im2_u = uint8(im2(pad+1:end-pad, pad+1:end-pad,:));
+imdff = double(im1_u) - double(im2_u);
 rmse = sqrt(mean(imdff(:).^2));
 psnr = 20*log10(255/rmse);
